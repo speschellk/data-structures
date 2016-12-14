7551224d16766b9a1f9917ba8380f18cd0462056
Hack Reactor Students <accounts@hackreactor.com>
2016-12-14 15:06:58 -0800

    all tests for functional passing

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -7,12 +7,20 @@ var Queue = function() {
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    storage[Object.keys(storage).length] = value;
   };
 
   someInstance.dequeue = function() {
+    var first = storage[0];
+    for (var i = Object.keys(storage).length - 1; i > 0; i--) {
+      storage[i - 1] = storage[i];
+    }
+    delete storage[Object.keys(storage).length - 1];
+    return first;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
--- sprint-one/src/functional/stack.js
+++ sprint-one/src/functional/stack.js
@@ -1,18 +1,29 @@
 var Stack = function() {
   var someInstance = {};
+  var keys;
 
   // Use an object with numeric keys to store values
   var storage = {};
 
   // Implement the methods below
   someInstance.push = function(value) {
+    keys = Object.keys(storage);
+    var index = keys.length;
+    storage[index] = value;
+    // storage[Object.keys(storage).length] = value;
   };
 
   someInstance.pop = function() {
+    keys = Object.keys(storage);
+    var last = storage[keys.length - 1];
+    delete storage[keys.length - 1];
+    return last;
   };
 
   someInstance.size = function() {
+    return Object.keys(storage).length;
   };
 
   return someInstance;
+
 };
