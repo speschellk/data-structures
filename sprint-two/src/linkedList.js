7e76795ce22e70680b7d1888fdb0a443fc49f092
Hack Reactor Students <accounts@hackreactor.com>
2016-12-14 21:12:51 -0800

    part 2: linkedList passes

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -5,30 +5,32 @@ var LinkedList = function() {
 
   list.addToTail = function(value) {
 
-    // refers to new node by its value
     var value = Node(value);
     
-    // if list doesn't have a tail yet
     if (list.tail === null) {
       list.tail = value;
       list.head = value;
-    // if list does have a tail
     } else {
       list.tail.next = value;
       list.tail = value;
     }
-    
   };
 
   list.removeHead = function() {
-    // identify the list.head
-    // find the next for list.head and point list.head to that new value
     var formerHead = list.head.value;
     list.head = list.head.next;
     return formerHead;
   };
 
-  list.contains = function(target) {
+  list.contains = function(target, node) {
+    node = node || list.head;
+
+    if (node.value === target) {
+      return true;
+    } else if (node.next !== null) {
+      return list.contains(target, node.next);
+    }
+    return false;
   };
 
   return list;
