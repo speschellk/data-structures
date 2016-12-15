9a0f500c104f781e479f184e574b77c40ec311b2
Hack Reactor Students <accounts@hackreactor.com>
2016-12-14 16:06:26 -0800

    pseudoclassical tests pass!!!

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,18 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Queue.prototype.enqueue = function(value) {
+  this.storage[Object.keys(this.storage).length] = value;
+};
 
+Queue.prototype.dequeue = function() {
+  var first = this.storage[0];
+  for (var i = Object.keys(this.storage).length - 1; i > 0; i--) {
+    this.storage[i - 1] = this.storage[i]; 
+  }
+  delete this.storage[Object.keys(this.storage).length - 1];
+  return first;
+};
+
+Queue.prototype.size = function() { return Object.keys(this.storage).length; };
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,18 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.storage = {};
 };
 
+Stack.prototype.push = function(value) {
+  this.storage[Object.keys(this.storage).length] = value;
+};
+
+Stack.prototype.pop = function () {
+  var last = this.storage[Object.keys(this.storage).length - 1];
+  delete this.storage[Object.keys(this.storage).length - 1];
+  return last;
+};
+
+Stack.prototype.size = function() {
+  return Object.keys(this.storage).length;
+};
 
