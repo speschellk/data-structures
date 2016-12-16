8726b69047478d7455e9935d31a423d1f052820f
Hack Reactor Students <accounts@hackreactor.com>
2016-12-16 15:09:10 -0800

    completed graph tests

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -1,39 +1,73 @@
 
-
 // Instantiate a new graph
 var Graph = function() {
+  this.newGraph = {};
 };
 
-// Add a node to the graph, passing in the node's value.
+// Time complexity - O(1)
+// We add in a value without needing to organize them in any order
 Graph.prototype.addNode = function(node) {
+  this.newGraph[node] = {};
 };
 
-// Return a boolean value indicating if the value passed to contains is represented in the graph.
+// Time complexity - O(1)
+// We check to see if the key exists. If it exists, we return true
 Graph.prototype.contains = function(node) {
+  // if (this.newGraph[node] !== undefined) {
+  //   return true;
+  // }
+  // return false;
+  return this.newGraph[node] !== undefined ? true : false;
 };
 
-// Removes a node from the graph.
+// Time complexity - O(n)
+// We need to iterate through all the keys to see if it has the edge of the deleted
 Graph.prototype.removeNode = function(node) {
+  // if (this.newGraph[node] !== undefined) {
+  //   delete this.newGraph[node];
+  // }
+  // for (var key in this.newGraph) {
+  //   if (this.newGraph[key][node] !== undefined) {
+  //     delete this.newGraph[key][node];
+  //   }
+  // }
+
+  if (this.newGraph[node] !== undefined) {
+    for (var key in this.newGraph[node]) {
+      delete this.newGraph[key][node];
+    }
+    delete this.newGraph[node];
+  }
 };
 
-// Returns a boolean indicating whether two specified nodes are connected.  Pass in the values contained in each of the two nodes.
+// Time complexity - O(1)
+// We always check a single point to see if it exists
 Graph.prototype.hasEdge = function(fromNode, toNode) {
+  return this.newGraph[fromNode][toNode] === '' ? true : false;
 };
 
-// Connects two nodes in a graph by adding an edge between them.
+// Time complexity - O(1)
+// We assign values in at a key, without iterating through a list
 Graph.prototype.addEdge = function(fromNode, toNode) {
+  this.newGraph[fromNode][toNode] = '';
+  this.newGraph[toNode][fromNode] = '';
 };
 
-// Remove an edge between any two specified (by value) nodes.
+// Time complexity - O(1)
+// We delete values, without needing to reorganize
 Graph.prototype.removeEdge = function(fromNode, toNode) {
+  delete this.newGraph[fromNode][toNode];
+  delete this.newGraph[toNode][fromNode];
 };
 
-// Pass in a callback which will be executed on each node of the graph.
+// Time complexity - O(n)
+// We iterate for all keys in the graph
 Graph.prototype.forEachNode = function(cb) {
+  for (var key in this.newGraph) {
+    cb(key);
+  }
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-
