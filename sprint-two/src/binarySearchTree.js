3a899506d5b2f32d28d455f6d528e7c0af9adbe1
Hack Reactor Students <accounts@hackreactor.com>
2016-12-16 11:13:56 -0800

    Part2: completed BinarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -7,27 +7,31 @@ var BinarySearchTree = function(value) {
 
 var bstTreeMethods = {};
 
+// Time complexity - O(log n)
+// Worst case scenario we need to go through half the tree
 bstTreeMethods.insert = function(value, node) {
   var child = {'value': value, 'left': null, 'right': null};
   _.extend(child, bstTreeMethods);
   if (node === undefined) {
     node = this;
   }
-  if (value >= node.value) {
-    if (node.right === null) {
-      node.right = child;
-    } else {
-      node.insert(value, node.right);
-    }
-  } else {
+  if (value < node.value) {
     if (node.left === null) {
       node.left = child;
     } else {
       node.insert(value, node.left);
     }
+  } else {
+    if (node.right === null) {
+      node.right = child;
+    } else {
+      node.insert(value, node.right);
+    }
   }
 };
 
+// Time complexity - O(log n)
+// Same as above, we will at most go through half
 bstTreeMethods.contains = function(target, node, found) {
   found = found || false;
   node = node || this;
@@ -36,46 +40,33 @@ bstTreeMethods.contains = function(target, node, found) {
     return true;
   } else {
 
-    if (target > node.value) {
-      if (node.right !== null) {
-        found = this.contains(target, node.right, found);
-      }
-    } else {
+    if (target < node.value) {
       if (node.left !== null) {
         found = this.contains(target, node.left, found);
+      }
+    } else {
+      if (node.right !== null) {
+        found = this.contains(target, node.right, found);
       }
     }
   }
   return found;
 };
 
+// Time complexity - O(n)
+// We NEED to go through every node, so it's linear
 bstTreeMethods.depthFirstLog = function(iterator, node) {
-  // need to start at the top and traverse down the tree via recursion
   node = node || this;
   iterator(node.value);
-  // if (target > node.value) {
-  //   if (node.right !== null) {
-  //     found = this.contains(target, node.right, found);
-  //   }
-  // } else {
-  //   if (node.left !== null) {
-  //     found = this.contains(target, node.left, found);
-  //   }
-  // }
+  if (node.left !== null) {
+    this.depthFirstLog(iterator, node.left);
+  }
+  if (node.right !== null) {
+    this.depthFirstLog(iterator, node.right);
+  }
+
 };
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-var array = [];
-var func = function(value) { array.push(value); };
-var bst = BinarySearchTree(5);
-bst.insert(2);
-bst.insert(3);
-
-bst.depthFirstLog(func);
-console.log(array);
-
-
-
