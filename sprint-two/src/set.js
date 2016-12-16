72cd9c4f54e2e579c5db2206895aaf556a03a839
Hack Reactor Students <accounts@hackreactor.com>
2016-12-16 15:20:01 -0800

    all set tests pass

--- sprint-two/src/set.js
+++ sprint-two/src/set.js
@@ -1,18 +1,29 @@
 var Set = function() {
   var set = Object.create(setPrototype);
-  set._storage = null; // fix me
+  set._storage = {};
   return set;
 };
 
 var setPrototype = {};
 
+// Time complexity - O(1)
+// We add the value into the object, without needing to organize the data
 setPrototype.add = function(item) {
+  this._storage[item] = item;
 };
 
+// Time complexity - O(1)
+// We check a single point and return true or false based on if it exists
 setPrototype.contains = function(item) {
+  return this._storage[item] !== undefined ? true : false;
 };
 
+// Time complexity - O(1)
+// We only check a single point and delete it if it exists
 setPrototype.remove = function(item) {
+  if (this._storage[item] !== undefined) {
+    delete this._storage[item];
+  }
 };
 
 /*
