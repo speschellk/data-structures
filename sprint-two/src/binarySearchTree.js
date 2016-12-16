a3d0dea40c36b5ecb0cba113b7dd17fe153f33a7
Hack Reactor Students <accounts@hackreactor.com>
2016-12-15 21:40:06 -0800

    fixed tree and created insert for BinarySearchTree

--- sprint-two/src/binarySearchTree.js
+++ sprint-two/src/binarySearchTree.js
@@ -1,7 +1,51 @@
 var BinarySearchTree = function(value) {
+  var newTree = {'value': value, 'left': null, 'right': null};
+  _.extend(newTree, bstTreeMethods);
+
+  return newTree;
 };
 
+var bstTreeMethods = {};
+
+bstTreeMethods.insert = function(value, node) {
+  var child = {'value': value, 'left': null, 'right': null};
+  _.extend(child, bstTreeMethods);
+  if (node === undefined) {
+    node = this;
+  }
+  if (value >= node.value) {
+    if (node.right === null) {
+      node.right = child;
+    } else {
+      node.insert(value, node.right);
+    }
+  } else {
+    if (node.left === null) {
+      node.left = child;
+    } else {
+      node.insert(value, node.left);
+    }
+  }
+};
+
+bstTreeMethods.contains = function(target) {
+
+};
+
+bstTreeMethods.depthFirstLog = function(cb) {
+
+};
 
 /*
  * Complexity: What is the time complexity of the above functions?
  */
+
+var bst = BinarySearchTree(5);
+bst.insert(2);
+bst.insert(3);
+bst.insert(7);
+bst.insert(6);
+console.log(bst);
+console.log(bst.right);
+
+
--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -47,15 +47,3 @@ treeMethods.contains = function(target, node, found) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-var tree = Tree(4);
-tree.addChild(5);
-tree.addChild(6);
-tree.children[0].addChild(7);
-tree.children[1].addChild(8);
-console.log(tree);
-console.log('find root?: ' + tree.contains(4));
-console.log('contain 5?: ' + tree.contains(5));
-console.log('contain 6?: ' + tree.contains(6));
-console.log('contain 7?: ' + tree.contains(7));
-console.log('contain 8?: ' + tree.contains(8));
-
