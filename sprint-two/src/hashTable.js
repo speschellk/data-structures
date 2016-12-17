72bb4716e8363b61397309539a69d6666148ff5b
Hack Reactor Students <accounts@hackreactor.com>
2016-12-17 09:55:39 -0800

    refactor hash table and completed remove

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -8,6 +8,7 @@ var HashTable = function() {
 // Time complexity - O(n)
 // We have to iterate through the entire hash (worst case) using the _.each method
 HashTable.prototype.insert = function(k, v) {
+  var blankKey;
   var found = false;
   var fn = this;
   var index = getIndexBelowMaxForKey(k, this._limit);
@@ -15,21 +16,24 @@ HashTable.prototype.insert = function(k, v) {
   if (this._storage.get(index) === undefined) {
     this._storage.set(index, [[k, v]]);
   } else {
-    // if the bucket, isn't empty and k is already existing
-    // for (var i = 0; i < this._storage.get(index).length; i++) {
-    //   if (this._storage.get(index)[i][0] === k) {
-    //     this._storage.get(index)[i] = [k, v];
-    //     found = true;
-    //   }
-    // }
     _.each(this._storage.get(index), function(value, i) {
       if (value[0] === k) {
         fn._storage.get(index)[i] = [k, v];
         found = true;
       }
+      // While iterating through, we want to see if there are any blank key values
+      // Keeping it within this each loop allows us to maintain a linear time complexity
+      if (value[0] === '') {
+        blankKey = i;
+      }
     });
     if (!found) {
-      this._storage.get(index).push([k, v]);
+      // We check if a blank key was found in this index and fill in the blank to reduce the number of tuples
+      if (blankKey !== undefined) {
+        this._storage.get(index)[blankKey] = [k, v];
+      } else {
+        this._storage.get(index).push([k, v]);
+      }
     }
   }
 };
@@ -39,13 +43,6 @@ HashTable.prototype.insert = function(k, v) {
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   var found;
-
-  // for (var i = 0; i < this._storage.get(index).length; i++) {
-  //   if (this._storage.get(index)[i][0] === k) {
-  //     return this._storage.get(index)[i][1];
-  //   }
-  // }
-
   _.each(this._storage.get(index), function(value, i) {
     if (value[0] === k) {
       found = value[1];
@@ -58,10 +55,16 @@ HashTable.prototype.retrieve = function(k) {
 // We iterate through the bucket (worst case)
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // this._storage.set(index, []);
+  /*
+  worst case scenario is if we constantly remove and remove all nodes
+  we would have a hash table with only blank values
+  However, we can maintain the time complexity for this by filtering out blank, ''
+  keys during resizing of the hash table. Since we need to go through each value anyway, 
+  we can filter out blank keys before choosing the size of the new table
+  */
   for (var i = 0; i < this._storage.get(index).length; i++) {
     if (this._storage.get(index)[i][0] === k) {
-      this._storage.get(index)[i] = 'remove';
+      this._storage.get(index)[i] = '';
     }
   }
 };
