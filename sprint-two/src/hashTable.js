54011685afa14f3c7721d4c207f6ee85882af884
Hack Reactor Students <accounts@hackreactor.com>
2016-12-16 20:18:49 -0800

    hash table - time complexity notes and refactor

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,46 +5,57 @@ var HashTable = function() {
   this._storage = LimitedArray(this._limit);
 };
 
+// Time complexity - O(n)
+// We have to iterate through the entire hash (worst case) using the _.each method
 HashTable.prototype.insert = function(k, v) {
-  // we create an index for 'k'
   var found = false;
+  var fn = this;
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // once the index is found, we can reference the storage with it
-  // if the bucket, storage(index), is completely empty
+
   if (this._storage.get(index) === undefined) {
     this._storage.set(index, [[k, v]]);
   } else {
     // if the bucket, isn't empty and k is already existing
-    for (var i = 0; i < this._storage.get(index).length; i++) {
-      if (this._storage.get(index)[i][0] === k) {
-        this._storage.get(index)[i] = [k, v];
+    // for (var i = 0; i < this._storage.get(index).length; i++) {
+    //   if (this._storage.get(index)[i][0] === k) {
+    //     this._storage.get(index)[i] = [k, v];
+    //     found = true;
+    //   }
+    // }
+    _.each(this._storage.get(index), function(value, i) {
+      if (value[0] === k) {
+        fn._storage.get(index)[i] = [k, v];
         found = true;
       }
-    }
+    });
     if (!found) {
       this._storage.get(index).push([k, v]);
     }
   }
-  
-  // see if the bucket is empty or filled
-    // if it's empty, just add a tuple [k, v]
-    // if not empty
-      // and doesn't exist, push into the bucket
-      // check to see if it already exists and replace the value
-      
-
 };
 
+// Time complexity - O(n)
+// We use _.each to iterate through the bucket (worst case)
 HashTable.prototype.retrieve = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
-  // debugger;
-  for (var i = 0; i < this._storage.get(index).length; i++) {
-    if (this._storage.get(index)[i][0] === k) {
-      return this._storage.get(index)[i][1];
+  var found;
+
+  // for (var i = 0; i < this._storage.get(index).length; i++) {
+  //   if (this._storage.get(index)[i][0] === k) {
+  //     return this._storage.get(index)[i][1];
+  //   }
+  // }
+
+  _.each(this._storage.get(index), function(value, i) {
+    if (value[0] === k) {
+      found = value[1];
     }
-  }
+  });
+  return found;
 };
 
+// Time complexity - O(n)
+// We iterate through the bucket (worst case)
 HashTable.prototype.remove = function(k) {
   var index = getIndexBelowMaxForKey(k, this._limit);
   // this._storage.set(index, []);
@@ -60,13 +71,3 @@ HashTable.prototype.remove = function(k) {
 /*
  * Complexity: What is the time complexity of the above functions?
  */
-
-// first test
-// var hashTable;
-// var people = [['Steven', 'Tyler'], ['George', 'Harrison'], ['Mr.', 'Doob'], ['Dr.', 'Sunshine'], ['John', 'Resig'], ['Brendan', 'Eich'], ['Alan', 'Turing']];
-
-
-// it('should not contain values that were not inserted', function() {
-//   hashTable.insert('Steven', 'Spielberg');
-//   expect(hashTable.retrieve('Steven')).not.to.equal('Seagal');
-// });
